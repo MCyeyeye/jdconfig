945f77144af377fb75caeeed25274e8a6a511d85
evanwoo766 <30330163+evan766@users.noreply.github.com>
2021-04-29 22:31:27 +0800

    Create custom_shell.sh

---
+++ custom_shell.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+#delete old run all task shell
+rm -rf /scripts/runall.sh
+
+#download run all task shell
+wget -P /scripts https://raw.githubusercontent.com/evan766/jdconfig/main/runall.sh
