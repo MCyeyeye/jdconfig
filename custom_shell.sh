b92d53f69b0b858b539b4b1f15090316960bb0f8
evanwoo766 <30330163+evan766@users.noreply.github.com>
2021-04-30 12:40:26 +0800

    Update custom_shell.sh

--- custom_shell.sh
+++ custom_shell.sh
@@ -5,3 +5,6 @@ rm -rf /scripts/runall.sh
 
 #download run all task shell
 wget -P /scripts https://raw.githubusercontent.com/evan766/jdconfig/main/runall.sh
+
+rm -rf /scripts/monk_pasture.js
+wget -P /scripts https://raw.githubusercontent.com/evan766/jdconfig/main/monk_pasture.js
