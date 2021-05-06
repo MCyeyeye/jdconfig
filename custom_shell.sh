5b6fc043ca1aa9ca411b81bb3e9eb48e1fdcddc6
evanwoo766 <30330163+evan766@users.noreply.github.com>
2021-05-06 15:04:59 +0800

    Update custom_shell.sh

--- custom_shell.sh
+++ custom_shell.sh
@@ -6,11 +6,11 @@ rm -rf /scripts/runall.sh
 #download run all task shell
 wget -P /scripts https://raw.githubusercontent.com/evan766/jdconfig/main/runall.sh
 
-rm -rf /scripts/monk_pasture.js
+#rm -rf /scripts/monk_pasture.js
 wget -P /scripts https://raw.githubusercontent.com/monk-coder/dust/dust/member/monk_pasture.js
 
-rm -rf  /scripts/z_shop_captain.js
+#rm -rf  /scripts/z_shop_captain.js
 wget -P /scripts https://raw.githubusercontent.com/monk-coder/dust/dust/i-chenzhe/z_shop_captain.js
 
-rm -rf  /scripts/jd_super_redrain.js
+#rm -rf  /scripts/jd_super_redrain.js
 wget -P /scripts https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js
