0e380adde22593be50dc13a550c1166b6390b146
evanwoo766 <30330163+evan766@users.noreply.github.com>
2021-05-10 00:00:44 +0800

    Update custom_shell.sh

--- custom_shell.sh
+++ custom_shell.sh
@@ -1,23 +1,23 @@
 #/bin/sh
 
 #delete old run all task shell
-rm -rf /scripts/runall.sh
+#rm -rf /scripts/runall.sh
 
 #download run all task shell
-wget -P /scripts https://raw.githubusercontent.com/evan766/jdconfig/main/runall.sh
+#wget -P /scripts https://raw.githubusercontent.com/evan766/jdconfig/main/runall.sh
 
 #rm -rf /scripts/monk_pasture.js
-wget -P /scripts https://raw.githubusercontent.com/monk-coder/dust/dust/member/monk_pasture.js
+#wget -P /scripts https://raw.githubusercontent.com/monk-coder/dust/dust/member/monk_pasture.js
 
 #rm -rf  /scripts/z_shop_captain.js
-wget -P /scripts https://raw.githubusercontent.com/monk-coder/dust/dust/i-chenzhe/z_shop_captain.js
+#wget -P /scripts https://raw.githubusercontent.com/monk-coder/dust/dust/i-chenzhe/z_shop_captain.js
 
 #rm -rf  /scripts/jd_super_redrain.js
-wget -P /scripts https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js
+#wget -P /scripts https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js
 
 #rm -rf  /scripts/jd_super_redrain.js
-wget -P /scripts https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js
+#wget -P /scripts https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js
 
-wget -P /scripts https://raw.githubusercontent.com/monk-coder/dust/dust/normal/adolf_oppo.js
+#wget -P /scripts https://raw.githubusercontent.com/monk-coder/dust/dust/normal/adolf_oppo.js
 
-wget -P /scripts https://raw.githubusercontent.com/monk-coder/dust/dust/car/adolf_haier.js
+#wget -P /scripts https://raw.githubusercontent.com/monk-coder/dust/dust/car/adolf_haier.js
