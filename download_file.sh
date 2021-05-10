2ba15898e15f4248557f80a73d8dd41c4948c113
huguohong <evan766@foxmail.com>
2021-05-10 23:34:50 +0800

    update

--- download_file.sh
+++ download_file.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
 # 脚本保存的路径 默认是 /scripts
-script_save_path="/scripts"
+save_path="/scripts"
+
+if [ "" != $1 ]; then
+    save_path=$1
+fi
 
 #远程脚本url数组
 scripts[0]="https://raw.githubusercontent.com/monk-coder/dust/dust/car/adolf_haier.js"
@@ -10,8 +15,8 @@ scripts[2]="https://raw.githubusercontent.com/monk-coder/dust/dust/car/adolf_ETI
 
 # 定义一个函数用于下载js脚本
 downloadFile(){
-    
     script_url=$1
+    script_save_path=$2
 
     script_name=${script_url##*/}
 
@@ -38,5 +43,9 @@ downloadFile(){
 # shellcheck disable=SC2068
 for url in ${scripts[@]}
 do
-    downloadFile $url
+    downloadFile $url "$save_path"
 done
+
+# 更新下载脚本
+downloadShellUrl="https://raw.githubusercontent.com/evan766/jdconfig/main/download_file.sh"
+downloadFile $downloadShellUrl "/scripts/docker"
